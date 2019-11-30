9d1125c47f97a86d8200b7607403984453765abe
jinfeihan57 <38484902+jinfeihan57@users.noreply.github.com>
2019-11-30 10:50:58 +0800

    Update Note.cc

--- Note.cc
+++ Note.cc
@@ -2,3 +2,4 @@
 /*使用CMAKE 进行项目管理编译的工程，使用cmake -DCMAKE_BUILD_TYPE=REALEASE 来设置编译版本为release版。*/
 /*Timsort 排序对于完全无序的数据效率没有 c++ 标准库的高，但是对于部分有序的序列效率较高，可以取代c++标准库中的stable_sort（稳定排序）。*/
 /*文件已 "r+" 模式打开对其进行读写时，操作类似内存中的额字节读写*/
+/*intel SIMD 指南 https://software.intel.com/sites/landingpage/IntrinsicsGuide/ */
