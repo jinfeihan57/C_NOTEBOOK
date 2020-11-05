c67402a73ee78390e1a192e5094b426191565086
jinfeihan57 <38484902+jinfeihan57@users.noreply.github.com>
2020-11-05 14:40:23 +0800

    Update Note.cc

--- Note.cc
+++ Note.cc
@@ -21,3 +21,6 @@ add gcc 选项-fno-stack-protector 修复 undefined reference to `__stack_chk_fa
  gcc -fsanitize=address -fno-omit-frame-pointer -fsanitize=leak -fsanitize=undefined  -g -O2 -static-libasan
 高压缩比算法
 http://mattmahoney.net/dc/zpaq_compression.pdf
+
+/* GNU gcc documentation */
+https://gcc.gnu.org/onlinedocs/
